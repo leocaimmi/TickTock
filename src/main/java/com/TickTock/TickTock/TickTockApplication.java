d78ed50e5a4ea6657354a2dc81856f35fb67942a
Leo-Dev <leonardocaimmi1@gmail.com>
2025-04-18 23:16:12 -0300

    proyecto spring boot init

---
+++ src/main/java/com/TickTock/TickTock/TickTockApplication.java
@@ -0,0 +1,14 @@
+package com.TickTock.TickTock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickTockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickTockApplication.class, args);
+	}
+	/*http://localhost:8080/swagger-ui/index.html |entrar a swagger
+	http://localhost:8080/h2-console | entrar en H2 bbdd*/
+}
---
+++ src/test/java/com/TickTock/TickTock/TickTockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TickTock.TickTock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickTockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
