e312f96a4b041ffa4049acd002b78ea8905fe1fb
Leo-Dev <leonardocaimmi1@gmail.com>
2025-05-10 20:29:36 -0300

    add documentacion con swagger y JWT

--- src/main/java/com/TickTock/TickTock/config/SwaggerConfig.java
+++ src/main/java/com/TickTock/TickTock/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.TickTock.TickTock.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -17,17 +20,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 public class SwaggerConfig {
-    // - - - - - Swagger config - - - - - //
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("API de TickTock")
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("API de TickTock")
                         .description("Documentación de la API")
                         .version("1.0.0")
-                        .license(new License().name("Apache 2.0").url("https://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Documentación de referencia")
-                        .url("https://swagger.io/docs/"));
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
     }
 }
